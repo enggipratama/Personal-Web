251fbab296d9fdce3701fef175e13f23ef636901
enggipratama <enggipratama82@gmail.com>
2023-04-13 06:38:34 +0700

    ubah logo & add button link discord

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
+// import reactLogo from './assets/react.svg'
+import Logo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={Logo} className="logo" alt="logo" />
         </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Selamat Datang</h1>
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
+        <a
+          href="https://discordapp.com/users/903595911840677889"
+          target="_blank"
+        >
+          <button variant="outlined">Add Me at DISCORD </button>
+        </a>
+        {/* <p>
           Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        </p> */}
       </div>
-      <p className="read-the-docs">
-        Enggi Pratama - 2023
-      </p>
+      <p className="footer">Enggi Pratama - 2023</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
